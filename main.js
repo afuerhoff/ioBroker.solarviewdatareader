5f24a348dca34d58ae5e1bb906712bbb2119fe24
root <achim.fuerhoff@outlook.de>
2019-07-06 19:51:43 +0200

    Abfrage der einzelnen Wechselrichter hinzugefügt

--- main.js
+++ main.js
@@ -15,11 +15,26 @@ const schedule = require('node-schedule');
 const telnet = require('telnet-client');
 var parser = require('cron-parser');
 
-var gthis; //Global verfügbar machen
+var gthis; 
 var sv_data;
 var sv_cmd = "00*";
-var sv_array = ['PV.', 'D0supply.', 'D0consumption.'];
 var conn;
+
+function calcChecksum(string) {
+	var buf = new Buffer(string);
+	// Calculate the modulo 256 checksum
+	var sum = 0;
+	for (var i = 0, l = buf.length; i < l; i++) {
+		sum = (sum + buf[i]) % 256;
+	}
+ 
+	//gthis.log.info('checksum (binary): ' + sum.toString(2));
+	// Convert to a two byte uppercase hex value
+	//var chars = sum.toString(16).toUpperCase();
+	//if (chars.length == 1) chars = "0" + chars;
+	//return chars;
+	return sum.toString(2);
+}
 
 class Solarviewdatareader extends utils.Adapter {
 
@@ -37,104 +52,91 @@ class Solarviewdatareader extends utils.Adapter {
 		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
 	}
-
+	
 	/**
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	async onReady() {
 		// Initialize your adapter here
-		gthis = this;
+		gthis = this; //global verfügbar machen
+		// Konfiguration lesen und als Info ausgeben
 		const ip_address = this.config.ipaddress;
 		const port = this.config.port;
 
 		this.log.info("ipaddress: " + ip_address);
 		this.log.info("port: " + this.config.port);
-		this.log.info("D0 converter: " + this.config.d0converter.toString());
-		//this.log.info(this.config.s0converter);
+		this.log.info("d0 converter: " + this.config.d0converter.toString());
 
 		/*
 		For every state in the system there has to be also an object of type state
 		*/
-		sv_array.forEach(async function(element) {
-			gthis.log.info("create object: " + element + "Actualy");
-			await gthis.setObjectAsync(element + "Actualy", {
-				type: "state",
-				common: {
-					name: "Actualy",
-					type: "number",
-					role: "indicator",
-					unit: "W",
-					read: true,
-					write: false,
-				},
-				native: {},
-			});
-			gthis.log.info("create object: " + element + "Daily");
-			await gthis.setObjectAsync(element + "Daily", {
-				type: "state",
-				common: {
-					name: "Daily",
-					type: "number",
-					role: "indicator",
-					unit: "kWh",
-					read: true,
-					write: false,
-				},
-				native: {},
-			});
-			gthis.log.info("create object: " + element + "Monthly");
-			await gthis.setObjectAsync(element + "Monthly", {
-				type: "state",
-				common: {
-					name: "Monthly",
-					type: "number",
-					role: "indicator",
-					unit: "kWh",
-					read: true,
-					write: false,
-				},
-				native: {},
-			});
-			gthis.log.info("create object: " + element + "Yearly");
-			await gthis.setObjectAsync(element + "Yearly", {
-				type: "state",
-				common: {
-					name: "Yearly",
-					type: "number",
-					role: "indicator",
-					unit: "kWh",
-					read: true,
-					write: false,
-				},
-				native: {},
-			});
-			gthis.log.info("create object: " + element + "Total");
-			await gthis.setObjectAsync(element + "Total", {
-				type: "state",
-				common: {
-					name: "Total",
-					type: "number",
-					role: "indicator",
-					unit: "kWh",
-					read: true,
-					write: false,
-				},
-				native: {},
-			});
-			gthis.log.info("create object: " + element + "LastUpdate");
-			await gthis.setObjectAsync(element + "LastUpdate", {
-				type: "state",
-				common: {
-					name: "LastUpdate",
-					type: "date",
-					role: "indicator",
-					unit: "",
-					read: true,
-					write: false,
-				},
-				native: {},
-			});			
-		});
+		
+		//Datenobjekte erzeugen
+		var arrTcp = [];
+		var arrTcpLen = arrTcp.push("pvg"); // PV Gesamtanlage
+		if (gthis.config.d0converter == true){ //d0converter hinzufügen
+			arrTcp.push("d0supply"); //Einspeisezähler
+			arrTcpLen = arrTcp.push("d0consumption"); //Verbrauchszähler
+		}
+		const arrDp1 = ['actualy', 'daily', 'monthly', 'yearly', 'total', 'lastupdate']; //Datenpunkte
+		const arrType1 = ['number', 'number', 'number', 'number', 'number', 'date']; //Datentypen
+		const arrUnit1 = ['W', 'kWh', 'kWh', 'kWh', 'kWh', '']; // Einheiten
+		var arrDpLen = arrDp1.length;
+		var i;
+		var dp;
+		for (i = 0; i < arrTcpLen; i++) { // normale Datenpunkte
+			for (dp = 0; dp < arrDpLen; dp++) {
+				gthis.log.info("create object: " + arrTcp[i] + "." + arrDp1[dp]); // Anlage Datenobjekte loggen
+				await gthis.setObjectAsync(arrTcp[i] + "." + arrDp1[dp], {
+					type: "state",
+					common: {
+						name: arrDp1[dp],
+						type: arrType1[dp],
+						role: "indicator",
+						unit: arrUnit1[dp],
+						read: true,
+						write: false,
+					},
+					native: {},
+				});
+			}				
+		}
+
+		var arrInv = [];
+		var inv;
+		for (inv = 1; inv < 5; inv++) { // zusätzliche Datenobjekte für Wechselrichter
+			if (eval("gthis.config.pvi" + inv) == true){
+				arrInv.push("pvi" + inv);
+			}
+		}
+		const arrInvLen = arrInv.length;
+
+		const arrDp2 = ['udc', 'idc', 'udcb', 'idcb', 'udcc', 'idcc', 'ul1', 'il1', 'ul2', 'il2', 'ul3', 'il3', 'tkk']; //zusätzliche Datenpunkte
+		const arrDp = arrDp1.concat(arrDp2);
+		arrDpLen = arrDp.length;
+		
+		const arrType2 = ['number', 'number', 'number', 'number', 'number', 'number', 'number', 'number', 'number', 'number', 'number', 'number', 'number']; //Datentypen für zusätzliche Datenpunkte
+		const arrType = arrType1.concat(arrType2);
+		const arrUnit2 = ['V', 'A', 'V', 'A', 'V', 'A', 'V', 'A', 'V', 'A', 'V', 'A', '°C']; //Einheiten für zusätzliche Datenpunkte
+		const arrUnit = arrUnit1.concat(arrUnit2);
+		gthis.log.info("len: " + arrInvLen);
+		for (i = 0; i < arrInvLen; i++) { // Datenpunkte für Wechselrichter anlegen
+			for (dp = 0; dp < arrDpLen; dp++) {
+				gthis.log.info("create object: " + arrInv[i] + "." + arrDp[dp]);
+				await gthis.setObjectAsync(arrInv[i] + "." + arrDp[dp], {
+					type: "state",
+					common: {
+						name: arrDp[dp],
+						type: arrType[dp],
+						role: "indicator",
+						unit: arrUnit[dp],
+						read: true,
+						write: false,
+					},
+					native: {},
+				});
+			}				
+		}		
 		
 		// in this template all states changes inside the adapters namespace are subscribed
 		this.subscribeStates("*");
@@ -142,10 +144,9 @@ class Solarviewdatareader extends utils.Adapter {
 		const starttime = this.config.intervalstart;
 		const endtime   = this.config.intervalend;
 
-		//var client;
-		//var client = netcat.client(port, ip_address);
 		conn = new telnet();
  
+		//telnet parameters
 		var params = {
 		  host: ip_address,
 		  port: port,
@@ -164,15 +165,15 @@ class Solarviewdatareader extends utils.Adapter {
 				const dnow = new Date();
 				var dstart = new Date(dnow.getFullYear() + "-" + (dnow.getMonth()+1) + "-" + dnow.getDate() + " " + starttime);
 				var dend = new Date(dnow.getFullYear() + "-" + (dnow.getMonth()+1) + "-" + dnow.getDate() + " " + endtime);
-				if (gthis.config.d0converter == true){
+				if (gthis.config.d0converter == true){ //Verbrauch wird immer eingelesen
 					setTimeout(function() {
 						sv_cmd = "22*";
 						conn.connect(params);
 						//client.start();
 					}, 20000);
 				}
-				if (dnow >= dstart && dnow <= dend ){
-					sv_cmd = "00*";
+				if (dnow >= dstart && dnow <= dend ){ //Einspeisung und Leistungsdaten werden nur im Interval eingelesen
+					sv_cmd = "00*"; //pvg
 					conn.connect(params);
 					//client.start();
 					if (gthis.config.d0converter == true){
@@ -181,6 +182,30 @@ class Solarviewdatareader extends utils.Adapter {
 							conn.connect(params);
 							//client.start();
 						}, 10000);
+					}
+					if (gthis.config.pvi1 == true){
+						setTimeout(function() {
+							sv_cmd = "01*"; //Wechselrichter 1
+							conn.connect(params);
+						}, 29000);
+					}
+					if (gthis.config.pvi2 == true){
+						setTimeout(function() {
+							sv_cmd = "02*";
+							conn.connect(params);
+						}, 38000);
+					}
+					if (gthis.config.pvi3 == true){
+						setTimeout(function() {
+							sv_cmd = "03*";
+							conn.connect(params);
+						}, 47000);
+					}
+					if (gthis.config.pvi4 == true){
+						setTimeout(function() {
+							sv_cmd = "04*";
+							conn.connect(params);
+						}, 56000);
 					}
 				}
 			});
@@ -200,21 +225,35 @@ class Solarviewdatareader extends utils.Adapter {
 				sv_data = sv_data.replace (/[{]+/,"");      // "{" entfernen
 				sv_data = sv_data.replace (/[}]+/,"");      // "}" entfernen
 				sv_data = sv_data.split(",");   			// split von sv_data in array
+				//Checksumme berechnen
 				var sv_prefix = "";
+				gthis.log.info("Len: " + sv_data.length);
+				//gthis.log.info("Checksumme: " + sv_data[sv_data.length-1]);
+				//var csum = calcChecksum(response.toString());
+				//gthis.log.info("Checksumme: " + csum);
 				switch(sv_data[0]){
-					case "00": sv_prefix = "PV.";
+					case "00": sv_prefix = "pvg.";
 					break;
-					case "21": sv_prefix = "D0supply.";
+					case "01": sv_prefix = "pvi1.";
 					break;
-					case "22": sv_prefix = "D0consumption.";
+					case "02": sv_prefix = "pvi2.";
+					break;
+					case "03": sv_prefix = "pvi3.";
+					break;
+					case "04": sv_prefix = "pvi4.";
+					break;
+					case "21": sv_prefix = "d0supply.";
+					break;
+					case "22": sv_prefix = "d0consumption.";
 					break;
 				}
 				//sv_data 00: WR, Tag, Monat, Jahr, Stunde, Minute, KDY, KMT, KYR, KT0,PAC, UDC, IDC, UDCB, IDCB, UDCC, IDCC, UL1, IL1, TKK
 				//{21,17,04,2015,16,21,0030.1,00459,001182,00001182,03290,000,000.0,000,000.0,000,000.0,000,000.0,00},!
 				// Tagesertrag= 30.1, Monatsertrag=495, Jahresertrag=1182, Gesamtertrag=1182 kWh., Leistung=3290W
+				
 				var value = Number(sv_data[10]);
-				gthis.setStateAsync(sv_prefix + "Actualy", { val: value, ack: true });
-				if (sv_prefix == "PV.") {
+				gthis.setStateAsync(sv_prefix + "actualy", { val: value, ack: true });
+				if (sv_prefix == "pv.") {
 				  if (gthis.config.setCCU == true){
 					gthis.log.info("write CCU system variable: " + gthis.config.CCUSystemV);
 					gthis.setForeignState(gthis.config.CCUSystemV,{ val: value, ack: false});				  
@@ -222,22 +261,59 @@ class Solarviewdatareader extends utils.Adapter {
 				}
 				
 				value = Number(sv_data[6]);
-				gthis.setStateAsync(sv_prefix + "Daily", { val: value, ack: true });
+				gthis.setStateAsync(sv_prefix + "daily", { val: value, ack: true });
 				
 				value = Number(sv_data[7]);
-				gthis.setStateAsync(sv_prefix + "Monthly", { val: value, ack: true });
+				gthis.setStateAsync(sv_prefix + "monthly", { val: value, ack: true });
 				
 				value = Number(sv_data[8]);
-				gthis.setStateAsync(sv_prefix + "Yearly", { val: value, ack: true });
+				gthis.setStateAsync(sv_prefix + "yearly", { val: value, ack: true });
 				
 				value = Number(sv_data[9]);
-				gthis.setStateAsync(sv_prefix + "Total", { val: value, ack: true });		
+				gthis.setStateAsync(sv_prefix + "total", { val: value, ack: true });		
 
 				var sDate = Number(sv_data[3]) + "-" + Number(sv_data[2]) + "-" + Number(sv_data[1]) + " " + Number(sv_data[4]) + ":" + Number(sv_data[5])
-				gthis.setStateAsync(sv_prefix + "LastUpdate", { val: sDate, ack: true });		
-
+				gthis.setStateAsync(sv_prefix + "lastupdate", { val: sDate, ack: true });		
+				
+				if (sv_prefix == 'pvi1.' || sv_prefix == 'pvi2.' || sv_prefix == 'pvi3.' || sv_prefix == 'pvi4.'){
+					value = Number(sv_data[11]);
+					gthis.setStateAsync(sv_prefix + "udc", { val: value, ack: true });		
+					value = Number(sv_data[12]);
+					gthis.setStateAsync(sv_prefix + "idc", { val: value, ack: true });		
+					value = Number(sv_data[13]);
+					gthis.setStateAsync(sv_prefix + "udcb", { val: value, ack: true });		
+					value = Number(sv_data[14]);
+					gthis.setStateAsync(sv_prefix + "idcb", { val: value, ack: true });		
+					value = Number(sv_data[15]);
+					gthis.setStateAsync(sv_prefix + "udcc", { val: value, ack: true });		
+					value = Number(sv_data[16]);
+					gthis.setStateAsync(sv_prefix + "idcc", { val: value, ack: true });	
+					if (sv_data.length == 27) { //neue Version Solarview
+						value = Number(sv_data[19]);
+						gthis.setStateAsync(sv_prefix + "ul1", { val: value, ack: true });		
+						value = Number(sv_data[20]);
+						gthis.setStateAsync(sv_prefix + "il1", { val: value, ack: true });		
+						value = Number(sv_data[21]);
+						gthis.setStateAsync(sv_prefix + "ul2", { val: value, ack: true });		
+						value = Number(sv_data[22]);
+						gthis.setStateAsync(sv_prefix + "il2", { val: value, ack: true });		
+						value = Number(sv_data[23]);
+						gthis.setStateAsync(sv_prefix + "ul3", { val: value, ack: true });		
+						value = Number(sv_data[24]);
+						gthis.setStateAsync(sv_prefix + "il3", { val: value, ack: true });		
+						value = Number(sv_data[25]);
+						gthis.setStateAsync(sv_prefix + "tkk", { val: value, ack: true });		
+					}
+					if (sv_data.length === 23) { //alte Version Solarview
+						value = Number(sv_data[19]);
+						gthis.setStateAsync(sv_prefix + "ul1", { val: value, ack: true });		
+						value = Number(sv_data[20]);
+						gthis.setStateAsync(sv_prefix + "il1", { val: value, ack: true });		
+						value = Number(sv_data[21]);
+						gthis.setStateAsync(sv_prefix + "tkk", { val: value, ack: true });							
+					}
+				}
 				gthis.log.info("connect: end" );    
-				//client.send();
 			}
 		  });
 		})
