7507b5264d7f56b0b7ac268da275ab5ea5139acd
afuerhoff <50245893+afuerhoff@users.noreply.github.com>
2024-06-01 21:02:09 +0200

    some changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,6 @@ module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     parserOptions: {
         ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-        project: './tsconfig.json',
     },
     extends: [
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
