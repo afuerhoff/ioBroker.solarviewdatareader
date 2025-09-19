5ecc301fddbbe72ee647d2057f6ba31b63a5b233
afuerhoff <50245893+afuerhoff@users.noreply.github.com>
2025-09-19 14:11:03 +0200

    typescript error fixed

--- src/main.ts
+++ src/main.ts
@@ -543,7 +543,7 @@ class Solarviewdatareader extends utils.Adapter {
     private async handleCCUUpdate(sv_data: string[]): Promise<void> {
         try {
             if (this.config.setCCU) {
-                const obj = await this.findForeignObjectAsync(this.config.CCUSystemV, 'state');
+                const obj = await this.findForeignObjectAsync(this.config.CCUSystemV);
                 if (obj && obj.name) {
                     this.log.debug(`set CCU system variable: ${this.config.CCUSystemV}`);
                     this.setForeignState(this.config.CCUSystemV, { val: Number(sv_data[10]), ack: false });
